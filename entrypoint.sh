21cfacbbfc94cfcf5c6ac324c63a7049019ea326
Cal Evans <cal@calevans.com>
2020-02-26 13:09:14 -0500

    Testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,16 +5,44 @@ cp /action/problem-matcher.json /github/workflow/problem-matcher.json
 echo "::add-matcher::${RUNNER_TEMP}/_github_workflow/problem-matcher.json"
 
 echo "Cal's test action:"
+echo "=================="
+echo " "
+echo " Environment Variables:"
+echo "======================="
 
 set
 
+echo " "
+echo " Mount Points :"
+echo "==============="
+
 df -h
+
+echo " "
+echo " Current Dir and contents :"
+echo "==========================="
+
+pwd
+echo " "
 
 ls -lh
 
-pwd
-
+echo " "
+echo " PHP info :"
+echo "==========================="
 php -i
+
+echo " "
+echo " Python info :"
+echo "==========================="
+python -version
+
+echo " "
+echo " IP info :"
+echo "==========================="
+ifconfig
+
+echo " "
 
 echo "::remove-matcher owner=phpcs::"
 
