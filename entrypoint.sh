a861868014fbd5730fd9ecd52825b412f81a5951
Cal Evans <cal@calevans.com>
2020-04-06 16:00:36 -0400

    commitmessage just wasn't working.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,15 +6,15 @@ $executingAction = getenv('GITHUB_ACTIONS')??false;
 if (!$executingAction) {
   $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
   $dotenv->load();
+  $commitMessage = getenv('COMMIT_MESSAGE');
 } else {
   exec("git log -n 1 --oneline --abbrev-commit --pretty=format:'%h--%d--%s' | awk -F '--' '{print $3}'", $commitMessage);
-  putenv('COMMIT_MESSAGE',$commitMessage);
 }
 
 $text = "Reference\n  " . getenv('GITHUB_REF') . "\n" .
         "Event\n  " . getenv('GITHUB_EVENT_NAME') . "\n" .
         "Commit\n  " . "https://github.com/" . getenv('GITHUB_REPOSITORY') . '/' . getenv('GITHUB_SHA') . "/commit\n" .
-        "Message\n  " . getenv('COMMIT_MESSAGE') . "\n";
+        "Message\n  " . $commitMessage . "\n";
 
 $payload = [
   'text' => 'Hello World ' . date('m-d-Y h:i:s'),
