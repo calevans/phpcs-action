e3aad07fb07fde18c2518d914aff0d69c1803c0e
Cal Evans <cal@calevans.com>
2020-04-06 19:52:45 -0400

    new format

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,53 +2,54 @@
 
 <?php
 require_once 'vendor/autoload.php';
-$executingAction = getenv('GITHUB_ACTIONS')??false;
+$executingAction = getenv('GITHUB_ACTIONS') ?? false;
 if (!$executingAction) {
   $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
   $dotenv->load();
-  $commitMessage = getenv('COMMIT_MESSAGE');
 } else {
   exec("git log -n 1 --oneline --abbrev-commit --pretty=format:'%h--%d--%s' | awk -F '--' '{print $3}'", $returnValue);
-  $commitMessage = $returnValue[0];
+  putenv('COMMIT_MESSAGE=' . $returnValue[0]);
 }
 
-$text = "*Reference*\n  " . getenv('GITHUB_REF') . "\n" .
-        "*Event*\n  " . getenv('GITHUB_EVENT_NAME') . "\n" .
-        "*Commit*\n  " . "https://github.com/" . getenv('GITHUB_REPOSITORY') . '/' . getenv('GITHUB_SHA') . "/commit\n" .
-        "*Message*\n  " . $commitMessage . "\n";
-
 $payload = [
-  'attachments ' => [
-    'text' =>'This is the attachment *TEXT* field.',
-    'mrkdwn_in' => ['text'],
-    'author_name' => getenv('SLACK_USERNAME'),
-    'title' => 'This is the attachment *TITLE* field',
-    'fields' => [
-      [
-        'title' => 'Reference',
-        'value' => getenv('GITHUB_REF'),
-        'short' => false
-      ],
-      [
-        'title' => 'Event',
-        'value' => getenv('GITHUB_EVENT_NAME'),
-        'short' => false
-      ],
-      [
-        'title' => 'Commit',
-        'value' => "https://github.com/" . getenv('GITHUB_REPOSITORY') . '/' . getenv('GITHUB_SHA') . "/commit\n",
-        'short' => false
-      ],
-      [
-        'title' => 'Message',
-        'value' =>  $commitMessage ,
-        'short' => false
-      ],
-    ]
-  ],
   'username' => getenv('SLACK_USERNAME'),
-  'icon_url' => getenv('SLACK_ICON')
+  'icon_url' => getenv('SLACK_ICON'),
+  'attachments' =>
+  [
+    [
+      'color' => getenv('SLACK_COLOR'),
+      'title' => getenv('GITHUB_REPOSITORY') . ' : ' . getenv('GITHUB_EVENT_NAME'),
+      'title_link' => 'https://github.com/' . getenv('GITHUB_REPOSITORY'), // needs more?
+      'text' => getenv('COMMIT_MESSAGE'),
+      'fields' =>
+      [
+        [
+          'title' => 'Repo',
+          'value' => 'https://github.com/' . getenv('GITHUB_REPOSITORY'),
+          'short' => false,
+        ],
+        [
+          'title' => 'Event',
+          'value' => getenv('GITHUB_EVENT_NAME'),
+          'short' => false,
+        ]
+      ],
+    ],
+  ],
 ];
 
+if (getenv('GITHUB_EVENT_NAME') !== 'pull_request') {
+  $payload['attachments'][0]['fields'][] = [
+    'title' => 'View the Actions',
+    'value' => 'https://github.com/' . getenv('GITHUB_REPOSITORY') . '/commit/' . getenv('GITHUB_SHA') . '/checks',
+    'short' => false,
+  ];
+
+}
+
 $client = new \GuzzleHttp\Client();
-$response = $client->request('POST', getenv('SLACK_WEBHOOK'),['json'=>$payload]);
+$response = $client->request('POST', getenv('SLACK_WEBHOOK'), ['json' => $payload]);
+
+if (!$executingAction) {
+  echo "\nStatus Code: " . $response->getStatusCode() . "\n";
+}
