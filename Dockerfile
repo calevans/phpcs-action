2cb9762232a09ef3355f01d98bc2438ae6de7623
Cal Evans <cal@calevans.com>
2020-04-06 18:31:18 -0400

    redo with slack attachments

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,6 @@ COPY entrypoint.sh \
      problem-matcher.json \
      /action/
 
-RUN composer global require guzzlehttp/guzzle
 RUN chmod +x /action/entrypoint.sh
 
 ENTRYPOINT ["/action/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/php
 
 <?php
-require_once '/root/.composer/vendor/autoload.php';
+require_once 'vendor/autoload.php';
 $executingAction = getenv('GITHUB_ACTIONS')??false;
 if (!$executingAction) {
   $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
@@ -18,7 +18,34 @@ $text = "*Reference*\n  " . getenv('GITHUB_REF') . "\n" .
         "*Message*\n  " . $commitMessage . "\n";
 
 $payload = [
-  'text' => $text,
+  'attachments ' => [
+    'text' =>'This is the attachment *TEXT* field.',
+    'mrkdwn_in' => ['text'],
+    'author_name' => getenv('SLACK_USERNAME'),
+    'title' => 'This is the attachment *TITLE* field',
+    'fields' => [
+      [
+        'title' => 'Reference',
+        'value' => getenv('GITHUB_REF'),
+        'short' => false
+      ],
+      [
+        'title' => 'Event',
+        'value' => getenv('GITHUB_EVENT_NAME'),
+        'short' => false
+      ],
+      [
+        'title' => 'Commit',
+        'value' => "https://github.com/" . getenv('GITHUB_REPOSITORY') . '/' . getenv('GITHUB_SHA') . "/commit\n",
+        'short' => false
+      ],
+      [
+        'title' => 'Message',
+        'value' =>  $commitMessage ,
+        'short' => false
+      ],
+    ]
+  ],
   'username' => getenv('SLACK_USERNAME'),
   'icon_url' => getenv('SLACK_ICON')
 ];
